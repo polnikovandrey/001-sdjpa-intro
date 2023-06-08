966b59a9f8eb6ef94a5d4aff7ee90f5dcb95fbad
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-08 11:44:45 +0300

    + Book Entity equals() & hashCode() methods

--- src/main/java/com/mfly/sdjpaintro/domain/Book.java
+++ src/main/java/com/mfly/sdjpaintro/domain/Book.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Objects;
+
 @Entity
 public class Book {
 
@@ -22,6 +24,21 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 
     public Long getId() {
